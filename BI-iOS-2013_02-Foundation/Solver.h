bb7ea2e03d83fa47ba52099c2fb345cc5ccf5106
Jakub Hladik <kubbing@me.com>
2013-09-30 10:32:54 +0200

    I cannot believe that it took this long to write a test for this.

---
+++ BI-iOS-2013_02-Foundation/Solver.h
@@ -0,0 +1,16 @@
+//
+//  Solver.h
+//  BI-iOS-2013_02-Foundation
+//
+//  Created by Jakub Hladík on 30.09.13.
+//  Copyright (c) 2013 FIT CTU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Solver : NSObject
+
+- (instancetype)init;
+- (instancetype)initWithA:(NSNumber *)a B:(NSNumber *)b c:(NSNumber *)c;
+
+@end
---
+++ BI-iOS-2013_02-Foundation/Solver.m
@@ -0,0 +1,33 @@
+//
+//  Solver.m
+//  BI-iOS-2013_02-Foundation
+//
+//  Created by Jakub Hladík on 30.09.13.
+//  Copyright (c) 2013 FIT CTU. All rights reserved.
+//
+
+#import "Solver.h"
+
+@implementation Solver
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        // setup
+    }
+    return self;
+}
+
+- (instancetype)initWithA:(NSNumber *)a
+                        B:(NSNumber *)b
+                        c:(NSNumber *)c
+{
+    self = [self init];
+    if (self) {
+        ;
+    }
+    return self;
+}
+
+@end
