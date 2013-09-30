1ef859b61aab99575ae1b5ba299d60a86a79582e
Jakub Hladik <kubbing@me.com>
2013-09-30 09:29:44 +0200

    ff

--- BI-iOS-2013_02-Foundation/AppDelegate.m
+++ BI-iOS-2013_02-Foundation/AppDelegate.m
@@ -15,7 +15,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor redColor];
     [self.window makeKeyAndVisible];
     
     
